a29c235940ec74e9114e07cdbeb483e34938ea17
Mqasim94 <149766611+Mqasim94@users.noreply.github.com>
2024-01-10 13:15:21 +0500

    EMS assignment uploaded

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+
+
+from django import forms
+from .models import*
+
+
+class EventForm(forms.ModelForm):
+
+    title = forms.CharField(max_length=200)
+    description = forms.CharField(widget=forms.Textarea)
+    date = forms.DateTimeField()
+    location = forms.CharField(max_length=200)
+
+class RegistrationForm(forms.ModelForm):
+
+    name = forms.CharField(max_length=200)
+    email = forms.EmailField()
